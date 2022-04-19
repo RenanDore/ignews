794d831936cedd4c45dc30c02e262a3cad7a116f
Renan Dore <dore.renan@gmail.com>
2022-04-19 11:52:40 -0300

    realizando login com a autenticacao do next e definindo preco no componente de subscricao

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import styles from "./styles.module.scss";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  //Renomeando o novo objeto chamado "data" para ter o nome "session" que é usado durante a aula:
+  const { data: session } = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Renan Dore
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
